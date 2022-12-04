56ad24f5a11a92b7306f0c5ce120847ea418b10f
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-12-04 13:13:36 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 // write your code here
-const menu = document.getElementById("ramen-menu")
+const menu = document.getElementById("ramen-menu");
 function fetchImages(url) {
-    fetchImages(url)
-    .then (resp => resp.json())
-    .then(ramen => {createImages(ramen)})
+  fetchImages(url)
+    .then((resp) => resp.json())
+    .then((ramen) => {
+      createImages(ramen);
+    });
 }
-fetchImages(" http://localhost:3000/ramens")
+fetchImages(" http://localhost:3000/ramens");
 
-function createImages(images){
-    for(let image of images){
-        newMenuItem(image)
-    }
+function createImages(images) {
+  for (let image of images) {
+    newMenuItem(image);
+  }
 }
-const form= document.getElementById("new-ramen")
-form.addEventListener("submit", (event)=>{
-    event.preventDefault()
-    let image = {
-      "image": document.querySelector("#new-image").value,
-     "name": document.querySelector("#new-image").value,
-     "restraunt": document.querySelector("#new-image").value,
-     "rating": document.querySelector("#new-image").value,
-      "comment":document.querySelector("#new-image").value,
-    }
-    newMenuItem(image)
-    showDetailInfo(image)
-})
-function newMenuItem(image){
-    let menuItem =document.createElement("img")
-    menuItem.setAttribute("src", image.image)
-    menu.appendChild(menuItem)
+const form = document.getElementById("new-ramen");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let image = {
+    image: document.querySelector("#new-image").value,
+    name: document.querySelector("#new-image").value,
+    restraunt: document.querySelector("#new-image").value,
+    rating: document.querySelector("#new-image").value,
+    comment: document.querySelector("#new-image").value,
+  };
+  newMenuItem(image);
+  showDetailInfo(image);
+});
+function newMenuItem(image) {
+  let menuItem = document.createElement("img");
+  menuItem.setAttribute("src", image.image);
+  menu.appendChild(menuItem);
 }
